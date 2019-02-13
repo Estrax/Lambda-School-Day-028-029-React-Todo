a2b44310352a0f6820e8078cf3326af4d46b7752
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-13 18:13:11 -0500

    updated search

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ class App extends React.Component {
 
 		this.state = {
 			todo: "",
-			todos: this.decode(this.localStorageFetch()) || []
+			todos: this.decode(this.localStorageFetch()) || [],
+			search: ""
 		};
 
 		this.addTodo = this.addTodo.bind(this);
 		this.completeTodo = this.completeTodo.bind(this);
 		this.clearCompleted = this.clearCompleted.bind(this);
-		this.onChange = this.onChange.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
+		this.onFormChange = this.onFormChange.bind(this);
+		this.onSearchChange = this.onSearchChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
 	}
 
 	encode(todos) {
@@ -32,6 +34,12 @@ class App extends React.Component {
 
 	localStorageFetch() {
 		return localStorage.getItem('todos');
+	}
+
+	findTodos(phrase) {
+		if(phrase === "") return this.state.todos;
+
+		return this.state.todos.filter(elem => elem.task.includes(phrase));
 	}
 
 	addTodo() {
@@ -71,7 +79,7 @@ class App extends React.Component {
 		});
 	};
 
-	onChange(event) {
+	onFormChange(event) {
 		this.setState({
 			...this.state,
 			todo: event.target.value,
@@ -79,7 +87,14 @@ class App extends React.Component {
 		});
 	}
 
-	onSubmit(event) {
+	onSearchChange(event) {
+		this.setState({
+			...this.state,
+			search: event.target.value
+		});
+	}
+
+	onFormSubmit(event) {
 		event.preventDefault();
 		this.addTodo();
 	}
@@ -89,8 +104,20 @@ class App extends React.Component {
 			<div>
 				<h1>Todo List</h1>
 				<h3>You have {this.state.todos.length} {this.state.todos.length === 1 ? "task" : "tasks"} on the todo list. {this.state.todos.filter(elem => !elem.completed).length > 0 ? `Still a lot to do. Keep working on the ${this.state.todos.filter(elem => !elem.completed).length} ${this.state.todos.length === 1 ? "task" : "tasks"} remaining!`: ""}</h3>
-				<TodoList todos={this.state.todos.sort((a, b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0))} completeTodo={this.completeTodo} />
-				<TodoForm todo={this.state.todo} addTodo={this.addTodo} update={this.onChange} submit={this.onSubmit} />
+				<input 
+					type="text"
+					name="search"
+					placeholder="find your todos..."
+					value={this.state.search}
+					onChange={this.onSearchChange}
+				/>
+				{
+					this.state.search !== "" ?
+					<h3>Found {this.findTodos(this.state.search).length} {this.findTodos(this.state.search).length === 1 ? "todo" : "todos"}.</h3>
+					: ""
+				}
+				<TodoList todos={this.findTodos(this.state.search).sort((a, b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0))} completeTodo={this.completeTodo} />
+				<TodoForm todo={this.state.todo} addTodo={this.addTodo} update={this.onFormChange} submit={this.onFormSubmit} />
 				<button onClick={this.clearCompleted}>Clear Completed</button>
 			</div>
 		);
