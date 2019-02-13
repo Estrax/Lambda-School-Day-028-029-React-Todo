a7d7833060db26c6abc406149f6a3cff814b734e
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-13 17:24:04 -0500

    refactored todoform to stateless component

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,48 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class TodoForm extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-			todo: ""
-		}
+const TodoForm = props => {
+	return (
+		<form onSubmit={props.submit}>
+			<input 
+				type="text"
+				name="todo"
+				placeholder="...todo"
+				value={props.todo}
+				onChange={props.update}
+			/>
 
-		this.onChange = this.onChange.bind(this);
-		this.onSubmit = this.onSubmit.bind(this);
-	}
-	
-	onChange(event) {
-		this.setState({
-			todo: event.target.value
-		});
-	}
-
-	onSubmit(event) {
-		event.preventDefault();
-		this.props.addTodo(this.state.todo);
-		this.setState({
-			todo: ""
-		})
-	}
-
-	render() {
-		return (
-			<form onSubmit={this.onSubmit}>
-				<input 
-					type="text"
-					name="todo"
-					placeholder="...todo"
-					value={this.state.todo}
-					onChange={this.onChange}
-				/>
-
-				<input
-					type="submit"
-					value="Add Todo"
-				/>
-			</form>
-		);
-	}
+			<input
+				type="submit"
+				value="Add Todo"
+			/>
+		</form>
+	);
 }
 
 export default TodoForm;
