ea4c2ccec08340e6b495f5c3001fe6e048e0468a
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-13 16:15:33 -0500

    added Todo form

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class TodoForm extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			todo: ""
+		}
+
+		this.onChange = this.onChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+	
+	onChange(event) {
+		this.setState({
+			todo: event.target.value
+		});
+	}
+
+	onSubmit(event) {
+		event.preventDefault();
+		this.props.addTodo(this.state.todo);
+		this.setState({
+			todo: ""
+		})
+	}
+
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.onSubmit}>
+					<input 
+						type="text"
+						name="todo"
+						placeholder="...todo"
+						value={this.state.todo}
+						onChange={this.onChange}
+					/>
+
+					<input
+						type="submit"
+						value="Add Todo"
+					/>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default TodoForm;
