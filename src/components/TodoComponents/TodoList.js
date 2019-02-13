7fc2e5aabcc0a40c7a5d38e8730d2880c5d08124
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-13 16:52:33 -0500

    refactored todolist to stateless component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,21 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
 
-import React, { Component } from 'react';
+import React from 'react';
 import Todo from './Todo';
 
-class TodoList extends Component {
-	constructor(props){
-		super(props);
-	}
-
-	render() {
-		return (
-			<div>
-				{this.props.todos.map(elem => <Todo todo={elem} completeTodo={this.props.completeTodo} key={elem.id} />)}
-			</div>
-		);
-	}
+const TodoList = props => {
+	return (
+		<div>
+			{props.todos.map(elem => <Todo todo={elem} completeTodo={props.completeTodo} key={elem.id} />)}
+		</div>
+	);
 }
 
 export default TodoList;
