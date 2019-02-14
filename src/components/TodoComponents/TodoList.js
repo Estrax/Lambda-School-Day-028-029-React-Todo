6393c29dce849e1f760d77aab6bb4285b44a01d2
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-14 13:20:24 -0500

    updated todolist

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -4,7 +4,7 @@ import Todo from './Todo';
 const TodoList = props => {
 	return (
 		<ul>
-			{props.todos.sort((a, b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0)).map(elem => <li key={elem.id}><Todo todo={elem} completeTodo={props.completeTodo} /></li>)}
+			{props.todos.sort((a, b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0)).map(elem => <li key={elem.id}><Todo todo={elem} completeTodo={props.completeTodo} key={elem.id-Date.parse('2019-01-01')}/></li>)}
 		</ul>
 	);
 }
