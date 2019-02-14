2028726ea81556be48a404a725236400f0391b57
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-02-13 20:35:08 -0500

    sort fix

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -4,11 +4,9 @@ import Todo from './Todo';
 const TodoList = props => {
 	return (
 		<ul>
-			{props.todos.map(elem => <li key={elem.id}><Todo todo={elem} completeTodo={props.completeTodo} /></li>)}
+			{props.todos.sort((a, b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0)).map(elem => <li key={elem.id}><Todo todo={elem} completeTodo={props.completeTodo} /></li>)}
 		</ul>
 	);
 }
 
 export default TodoList;
-
-//.sort((a, b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0))
